ae53efa77aab6ef3e79d50124d4da5729cf05465
Luiza Raczyło <luiza.raczylo@gmail.com>
2022-10-16 22:10:52 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+const throttle = require(`lodash.throttle`);
 
+const form = document.querySelector('.feedback-form');
+const emailLabel = document.querySelector('input');
+const messageLabel = document.querySelector('textarea');
+const data = {
+        email: emailLabel.value,
+        message: messageLabel.value,
+    };
+
+form.addEventListener('input', throttle(inputData, 500));
+
+function inputData() {
+    localStorage.setItem(`feedback-form-state`, JSON.stringify(data));
+}
+if (localStorage === " ") {
+    emailLabel.value = JSON.parse(localStorage.getItem(`feedback-form-state`)).email;
+    messageLabel.value = JSON.parse(localStorage.getItem(`feedback-form-state`)).message;
+}
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+    e.preventDefault();
+    const { elements: { email, message }, } = e.currentTarget;
+    if (email.value === `` || message.value === ``) {
+        return alert(`Please complete all fields!`);
+    } else {
+        const result = { email: email.value, message: message.value };
+        console.log(result);
+        localStorage.removeItem(`feedback-form-state`);
+    }
+}
