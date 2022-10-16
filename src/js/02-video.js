8ebbfb4ac5cff8da5fdf0ad869d8fff9692e5ed2
Luiza Raczyło <luiza.raczylo@gmail.com>
2022-10-16 19:07:36 +0200

    commit 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+const throttle = require(`lodash.throttle`);
+import Player from "@vimeo/player";
+
+const iframe = document.querySelector('#vimeo-player');
+const iframePlayer = new Player(iframe);
+
+const startPlay = parseFloat(localStorage.getItem(`videoplayer-current-time`)) || 0;
+
+iframePlayer.setCurrentTime(startPlay);
+
+function secondsPlay(data) {
+    localStorage.setItem(`videoplayer-current-time`, data.seconds);
+    console.log(`Time update: ` + localStorage.getItem(`videoplayer-current-time`));
+}
+iframePlayer.on(`timeupdate`, throttle(secondsPlay, 1000));
 
